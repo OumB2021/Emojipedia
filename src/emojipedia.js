504eeca00ac70177eb522ac441864cc5a3825ba9
Oumar <baarryoumar@gmail.com>
2023-03-01 00:21:35 -0500

    Challenge done

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -20,6 +20,28 @@ const emojipedia = [
     meaning:
       "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
   },
+
+  {
+    id: 4,
+    emoji: "💓",
+    name: "Beating Heart",
+    meaning:
+      "A heart with vibration/movement lines above it or around it, indicating it is beating. Displayed in pink or red on most platforms.",
+  },
+  {
+    id: 5,
+    emoji: "👀",
+    name: " Eyes",
+    meaning:
+      "A pair of eyes, glancing slightly to the left on most platforms. The Google version of this emoji previously showed only a single eye, despite the name of this character being eyes (plural).",
+  },
+  {
+    id: 4,
+    emoji: "🔥",
+    name: "Fire",
+    meaning:
+      "A flame, as produced when something is on fire. Depicted as a red, orange, and yellow flickering flame. Commonly used for various metaphorical expressions related to fire, including the slang hot and lit",
+  },
 ];
 
 export default emojipedia;
